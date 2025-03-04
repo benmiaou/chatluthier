c5182f53ede381c50035b70c18aca83eca809671
Benmiaou <benj.thibault@gmail.com>
2025-03-04 19:39:12 +0100

    update srv for limit

--- srv/app.js
+++ srv/app.js
@@ -7,10 +7,10 @@ const soundRoutes = require('./routes/soundRoutes');
 const requestRoutes = require('./routes/requestRoutes');
 
 const app = express();
-
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
